6a35590756863a5c84315b66d099523b704f2c46
Rodrigo de Almeida Pereira <contato@rodrigo-pereira.net>
2016-04-14 16:19:50 -0300

    fix before restore and real destroy callbacks

--- test/paranoia_test.rb
+++ test/paranoia_test.rb
@@ -27,6 +27,8 @@ def setup!
     'plain_models' => 'deleted_at DATETIME',
     'callback_models' => 'deleted_at DATETIME',
     'fail_callback_models' => 'deleted_at DATETIME',
+    'fail_restore_callback_models' => 'deleted_at DATETIME',
+    'fail_real_destroy_callback_models' => 'deleted_at DATETIME',
     'related_models' => 'parent_model_id INTEGER, parent_model_with_counter_cache_column_id INTEGER, deleted_at DATETIME',
     'asplode_models' => 'parent_model_id INTEGER, deleted_at DATETIME',
     'employers' => 'name VARCHAR(32), deleted_at DATETIME',
@@ -464,6 +466,18 @@ class ParanoiaTest < test_framework
     model.reload
 
     assert model.instance_variable_get(:@restore_callback_called)
+  end
+
+  def test_fail_restore_callback
+    model = FailRestoreCallbackModel.create
+    assert model.destroy
+
+    assert_equal false, model.restore
+  end
+
+  def test_fail_real_destroy_callback
+    model = FailRealDestroyCallbackModel.create
+    assert_equal false, model.really_destroy!
   end
 
   def test_really_destroy
@@ -949,6 +963,30 @@ class FailCallbackModel < ActiveRecord::Base
   acts_as_paranoid
 
   before_destroy { |_| false }
+end
+
+class FailRestoreCallbackModel < ActiveRecord::Base
+  acts_as_paranoid
+
+  before_restore { |_|
+    if ActiveRecord::VERSION::MAJOR < 5
+      false
+    else
+      throw :abort
+    end
+  }
+end
+
+class FailRealDestroyCallbackModel < ActiveRecord::Base
+  acts_as_paranoid
+
+  before_real_destroy { |_|
+    if ActiveRecord::VERSION::MAJOR < 5
+      false
+    else
+      throw :abort
+    end
+  }
 end
 
 class FeaturefulModel < ActiveRecord::Base
